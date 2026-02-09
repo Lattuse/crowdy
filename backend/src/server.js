88b224dd20f46091d8538e1925fe84160e1bd3aa
Lattuse <sultik280805@gmail.com>
2026-02-09 23:23:24 +0500

    deployment settings

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,6 +3,7 @@ const app = require("./app");
 const { connectDB } = require("./config/db");
 
 const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => console.log("Server on", PORT));
 
 connectDB(process.env.MONGO_URI)
   .then(() => app.listen(PORT, () => console.log("Server running on", PORT)))

ad601306b32a8a4be2eae82f8c8901e65eb42e6d
Lattuse <sultik280805@gmail.com>
2026-02-09 23:30:51 +0500

    cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -20,7 +20,12 @@ console.log("subscriptionRoutes", typeof subscriptionRoutes);
 console.log("creatorRoutes", typeof creatorRoutes);
 
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: [process.env.FRONTEND_URL],
+    credentials: true,
+  }),
+);
 app.use(express.json());
 app.use(morgan("dev"));
 //app.use("/uploads", express.static("uploads"));

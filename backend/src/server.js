10a993a158fd200b949b762ece36ef90b96f60d3
Lattuse <sultik280805@gmail.com>
2026-04-01 18:43:22 +0500

    app listen fix

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,10 +3,11 @@ const app = require("./app");
 const { connectDB } = require("./config/db");
 
 const PORT = process.env.PORT || 8080;
-app.listen(PORT, "0.0.0.0", () => console.log("Server on", PORT));
 
 connectDB(process.env.MONGO_URI)
-  .then(() => app.listen(PORT, () => console.log("Server running on", PORT)))
+  .then(() => {
+    app.listen(PORT, "0.0.0.0", () => console.log("Server running on", PORT));
+  })
   .catch((e) => {
     console.error(e);
     process.exit(1);

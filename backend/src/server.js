fcee7702893ceb2a528937a441a651e46cbf879d
Lattuse <sultik280805@gmail.com>
2026-04-01 18:37:55 +0500

    port change

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,8 +2,8 @@ require("dotenv").config();
 const app = require("./app");
 const { connectDB } = require("./config/db");
 
-const PORT = process.env.PORT || 4000;
-app.listen(PORT, () => console.log("Server on", PORT));
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, "0.0.0.0", () => console.log("Server on", PORT));
 
 connectDB(process.env.MONGO_URI)
   .then(() => app.listen(PORT, () => console.log("Server running on", PORT)))
